53485ff80a13738695f793b99ca8246dc173f080
hermite2053 <61319918+hermite2053@users.noreply.github.com>
2022-08-03 00:29:45 +0900

    Add numbers

---
+++ informal/numbers/floating_point.py
@@ -0,0 +1,2 @@
+print('2.0 + 3.0 =', 2.0 + 3.0)
+print('1 + 4 * 1.5 =', 1 + 4 * 1.5) # convert the int-type to float-type
---
+++ informal/numbers/operators.py
@@ -0,0 +1,8 @@
+print('5 + 2 =', 5 + 2)
+print('5 - 2 =', 5 - 2)
+print('5 * 2 =', 5 * 2)
+print('5 / 2 =', 5 / 2)
+
+print('5 // 2 =', 5 // 2)  # floor dividion
+print('5 % 2 =', 5 % 2)  # remainder of the division
+print('5 ** 2 =', 5 ** 2) # power
