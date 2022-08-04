f29d873a0388dda475a17a43f72b2b3dfdb7506f
hermite2053 <61319918+hermite2053@users.noreply.github.com>
2022-08-04 23:49:22 +0900

    [wip] add lists

---
+++ informal/lists/lists.py
@@ -0,0 +1,2 @@
+prime = [2, 3, 5, 7, 11, 13]
+print(prime)
---
+++ informal/lists/slice.py
@@ -0,0 +1,7 @@
+prime = [2, 3, 5, 7, 11, 13]
+print('prime[:]:', prime[:])
+print('prime[2:4]:', prime[2:4])
+print('prime[3:]:', prime[3:])
+print('prime[:5]:', prime[:5])
+print('prime[-5:-3]:', prime[-5:-3])
+print('prime[:-2]:', prime[:-2])
