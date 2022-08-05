8bcb02867c951dc96faa81ff979205c121e3e0b4
hermite2053 <61319918+hermite2053@users.noreply.github.com>
2022-08-05 22:33:31 +0900

    add lists

---
+++ informal/lists/nested_list.py
@@ -0,0 +1,10 @@
+array = [['a', 'b', 'c'], ['d', 'e', 'f']]
+print('array =', array)
+print('array[0][0] =', array[0][0])
+print('array[0][1] =', array[0][1])
+print('array[0][2] =', array[0][2])
+print('array[1][0] =', array[1][0])
+print('array[1][1] =', array[1][1])
+print('array[1][2] =', array[1][2])
+print('array[0] =', array[0])
+print('array[1] =', array[1])
