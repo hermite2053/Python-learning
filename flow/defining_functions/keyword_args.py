59c6ebe830e6ea11f378da300f2cd5715c829bd6
hermite2053 <61319918+hermite2053@users.noreply.github.com>
2022-08-08 21:27:07 +0900

    Add defining functions.

---
+++ flow/defining_functions/keyword_args.py
@@ -0,0 +1,5 @@
+def print_distance(value, unit='meters'):
+    print("Distance:", value, unit)
+
+print_distance(299792458) # is equal to light travels in 1 second.
+print_distance(220, 'yards') # is equal to 1 Furlong, used in horse racing.
